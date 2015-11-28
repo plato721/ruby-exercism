73fd117ff772e3e736cfecf86b1805fa74439609
Matt Ewell <matt.ewell@gmail.com>
2015-11-28 14:49:55 -0700

    make key random

--- simple-cipher/simple_cipher.rb
+++ simple-cipher/simple_cipher.rb
@@ -1,5 +1,8 @@
 class Cipher
-  def initialize(arg="aaaa")
+  attr_reader :key
+
+  def initialize(key = nil)
+    @key = key || init_key
   end
 
   def encode(plain)
@@ -22,11 +25,16 @@ class Cipher
     char.ord + key_at(index)
   end
 
+  private
   def key_at(index)
     key[index].ord - 'a'.ord
   end
 
-  def key
-    "dddddddddd"
+  def init_key
+    [*1..10].map { random_character }.join
+  end
+
+  def random_character
+    [*'a'..'z'].shuffle.pop
   end
 end
