6d36c06213fb42ad6c08982eca94c5c4224b58a0
Matt Ewell <matt.ewell@gmail.com>
2015-11-29 23:31:12 -0700

    kindergarten garden complete

--- kindergarten-garden/kindergarten_garden.rb
+++ kindergarten-garden/kindergarten_garden.rb
@@ -1,8 +1,39 @@
 class Garden
-  def initialize(arg)
+  attr_reader :garden, :students
+
+  def initialize(info, students=[*'a'..'z'])
+    @garden = garden_init(info)
+    @students = students_init(students)
   end
 
-  def alice
-    [:radishes, :clover, :grass, :grass]
+  def plant_map
+    { 'R' => :radishes,
+      'C' => :clover,
+      'G' => :grass,
+      'V' => :violets }
   end
+
+  def garden_init(info)
+    string_rows = info.split("\n")
+    [ string_rows[0].chars, string_rows[1].chars ]
+  end
+
+  def students_init(students)
+    students.sort.map { |student| student[0].downcase }
+  end
+
+  def codes_at(child_num)
+    first_plant = child_num * 2
+    [ self.garden.first[first_plant], self.garden.first[first_plant + 1],
+      self.garden.last[first_plant], self.garden.last[first_plant + 1] ]
+  end
+
+  def position_for(name)
+    students.index(name.to_s.downcase[0])
+  end
+
+  def method_missing(m)
+    codes_at(position_for(m)).map { |code| plant_map[code] }
+  end
+
 end
