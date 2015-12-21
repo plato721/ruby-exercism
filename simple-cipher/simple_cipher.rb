1027a505b6bce8e3f782b772f80d2e18b7a69067
Matt Ewell <matt.ewell@gmail.com>
2015-12-21 16:38:41 -0700

    Add simple-cipher

--- simple-cipher/simple_cipher.rb
+++ simple-cipher/simple_cipher.rb
@@ -6,17 +6,6 @@ class Cipher
     @key = key || init_key
   end
 
-  def check_key(key)
-    bad_key if key.empty?
-    key.chars.each do |char|
-      bad_key if !(char =~ /[a-z]{1}/)
-    end
-  end
-
-  def bad_key
-    raise ArgumentError, "Key must be all lowercase letters"
-  end
-
   def encode(plain)
     plain.chars.map.with_index do |char, index|
       encode_single(char, index).chr
@@ -29,15 +18,27 @@ class Cipher
     end.join
   end
 
+  private
+  def check_key(key)
+    bad_key if key.empty?
+    key.chars.each do |char|
+      bad_key if !(char =~ /[a-z]{1}/)
+    end
+  end
+
+  def bad_key
+    raise ArgumentError, "Key must be all lowercase letters"
+  end
+
   def decode_single(char, index)
-    char.ord - key_at(index)
+    raw = char.ord - key_at(index)
+    raw < 'a'.ord ? raw += 26 : raw 
   end
 
   def encode_single(char, index)
-    char.ord + key_at(index)
+    (char.ord - 'a'.ord + key_at(index)) % 26 + 'a'.ord
   end
 
-  private
   def key_at(index)
     key[index].ord - 'a'.ord
   end
@@ -47,6 +48,6 @@ class Cipher
   end
 
   def random_character
-    [*'a'..'z'].shuffle.pop
+    [*'a'..'z'].sample
   end
 end
--- simple-cipher/simple_cipher_test.rb
+++ simple-cipher/simple_cipher_test.rb
@@ -59,39 +59,33 @@ class SubstitutionCipherTest < Minitest::Test
   end
 
   def test_cipher_key_is_as_submitted
-    skip
     assert_equal(@cipher.key, @key)
   end
 
   def test_cipher_encode
-    skip
     plaintext = 'aaaaaaaaaa'
     ciphertext = 'abcdefghij'
     assert_equal(ciphertext, @cipher.encode(plaintext))
   end
 
   def test_cipher_decode
-    skip
     plaintext = 'aaaaaaaaaa'
     ciphertext = 'abcdefghij'
     assert_equal(plaintext, @cipher.decode(ciphertext))
   end
 
   def test_cipher_reversible
-    skip
     plaintext = 'abcdefghij'
     assert_equal(plaintext, @cipher.decode(@cipher.encode(plaintext)))
   end
 
   def test_double_shift_encode
-    skip
     plaintext = 'iamapandabear'
     ciphertext = 'qayaeaagaciai'
     assert_equal(ciphertext, Cipher.new('iamapandabear').encode(plaintext))
   end
 
   def test_cipher_encode_wrap
-    skip
     plaintext = 'zzzzzzzzzz'
     ciphertext = 'zabcdefghi'
     assert_equal(ciphertext, @cipher.encode(plaintext))
@@ -104,21 +98,18 @@ class PseudoShiftCipherTest < Minitest::Test
   end
 
   def test_cipher_encode
-    skip
     plaintext = 'aaaaaaaaaa'
     ciphertext = 'dddddddddd'
     assert_equal(ciphertext, @cipher.encode(plaintext))
   end
 
   def test_cipher_decode
-    skip
     plaintext = 'aaaaaaaaaa'
     ciphertext = 'dddddddddd'
     assert_equal(plaintext, @cipher.decode(ciphertext))
   end
 
   def test_cipher_reversible
-    skip
     plaintext = 'abcdefghij'
     assert_equal(plaintext, @cipher.decode(@cipher.encode(plaintext)))
   end
