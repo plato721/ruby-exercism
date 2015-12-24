205c9d5cf2328e86822952d64e51a02e56f2dc3b
Matt Ewell <matt.ewell@gmail.com>
2015-12-24 14:55:13 -0700

    Use min in triples_for and avoid separate check against min

--- pythagorean-triplet/pythagorean_triplet.rb
+++ pythagorean-triplet/pythagorean_triplet.rb
@@ -19,10 +19,13 @@ class Triplet
     self.a**2 + self.b**2 == self.c**2
   end
 
-  def self.triples_for(max)
-    [*1..max].each_with_object([]) do |c, triples|
-      [*1..max].each do |b|
-        [*1..max].each do |a|
+  def self.triples_for(args)
+    max = args[:max_factor]
+    min = args[:min_factor] || 1
+
+    [*min..max].each_with_object([]) do |c, triples|
+      [*min..max - 1].each do |b|
+        [*min..max - 2].each do |a|
           triple = new(a, b, c)
           triples << triple if triple.pythagorean?
         end
@@ -36,16 +39,6 @@ class Triplet
     end
   end
 
-  def self.above_min?(triple, min)
-    triple.a > min && triple.b > min && triple.c > min
-  end
-
-  def self.apply_min(triples, min)
-    triples.each_with_object([]) do |triple, min_checked|
-      min_checked << triple if above_min?(triple, min)
-    end
-  end
-
   def self.apply_sum(triples, sum)
     triples.each_with_object([]) do |triple, sum_checked|
       sum_checked << triple if triple.sum == sum
@@ -54,9 +47,8 @@ class Triplet
 
   def self.where(args)
     check_max_factor(args)
-    triples = triples_for(args[:max_factor])
+    triples = triples_for(args)
     triples = remove_dups(triples)
-    triples = apply_min(triples, args[:min_factor]) if args[:min_factor]
     triples = apply_sum(triples, args[:sum]) if args[:sum]
     triples
   end
