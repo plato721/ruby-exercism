425d4c04cfcb505994e5fd3bda1feed8c68b6a78
Matt Ewell <matt.ewell@gmail.com>
2015-12-24 22:08:42 -0700

    Complete largest series product

--- largest-series-product/largest_series_product.rb
+++ largest-series-product/largest_series_product.rb
@@ -5,7 +5,24 @@ class Series
     @sequence = sequence.chars.map(&:to_i)
   end
 
+  def slice_valid?(length)
+    length <= sequence.length
+  end
+
+  def validate_slice(length)
+    if !slice_valid?(length)
+      raise ArgumentError,
+        "Must choose slice at most length of sequence (#{sequence.length})"
+    end
+  end
+
   def largest_product(length)
-    sequence.inject(:*)
+    validate_slice(length)
+    max = 1
+    sequence.each_with_index do |num, idx|
+      cur = sequence.slice(idx...(idx + length)).inject(:*)
+      max = cur if cur > max
+    end
+    max
   end
 end
