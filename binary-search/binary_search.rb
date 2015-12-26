3d76cca4d93c14ea5bb48a8a8ba42e14279ce040
Matt Ewell <matt.ewell@gmail.com>
2015-12-26 10:42:15 -0700

    All tests passing

--- binary-search/binary_search.rb
+++ binary-search/binary_search.rb
@@ -1,5 +1,6 @@
 class BinarySearch
   attr_reader :list
+  attr_accessor :current_list
 
   def initialize(list)
     check_sorted(list)
@@ -13,7 +14,7 @@ class BinarySearch
   end
 
   def search_for(num)
-    location = search(num)
+    location = search(num: num, lower: 0, upper: list.length - 1)
     location ? location : not_found
   end
 
@@ -21,18 +22,42 @@ class BinarySearch
     raise "The search value is not in the list"
   end
 
-  def search(num, list = self.list)
-    # first = list.first
-    # last = list.last
-    # mid = (first + last) / 2
-    # return mid if list[mid] == num
-    # return search(num, list.slice(first, mid - 1)) if list[mid] > num
-    # return search(num, list.slice(mid + 1, last)) if list[mid] < num
-    nil
+
+  def lower_options(args)
+    mid = (args[:lower] + args[:upper]) / 2
+    args[:upper] = mid - 1
+    return nil if args[:upper] < args[:lower]
+    args
+  end
+
+  def upper_options(args)
+    mid = (args[:lower] + args[:upper]) / 2
+    args[:lower] = mid + 1
+    return nil if args[:lower] > args[:upper]
+    args
+  end
+
+  def get_search_options(args)
+    mid = (args[:lower] + args[:upper]) / 2
+    return lower_options(args) if args[:num] < list[mid]
+    return upper_options(args) if args[:num] > list[mid]
+  end
+
+  def search(args)
+    mid = (args[:lower] + args[:upper]) / 2
+    return mid if list[mid] == args[:num]
+    new_options = get_search_options(args)
+    return nil if !new_options
+    search(new_options)
   end
 
 
   def sorted?(array)
     array.sort == array
+  end
+
+  # Totally worthless function
+  def middle
+    midpoint = list.length / 2
   end
 end
