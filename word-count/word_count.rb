487389247ea32bf0227c619779ad159558935d13
Matt Ewell <matt.ewell@gmail.com>
2015-12-27 12:38:38 -0700

    Remove unnecessary variable declarations words array and counting hash

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,16 +1,12 @@
 class Phrase
   def initialize(phrase)
     @phrase = phrase
-    @word_count = Hash.new(0)
   end
   def word_count
-    @word_count.clear
-    words = []
     words = @phrase.downcase.split(/[ \n,]/)
-    words.each do |word|
+    words.each_with_object(Hash.new(0)) do |word, words_count|
       scrubbed_word = word.match(/[\w']+/).to_s
-      @word_count[scrubbed_word] += 1 if scrubbed_word.length > 0
+      words_count[scrubbed_word] += 1 if scrubbed_word.length > 0
     end
-    @word_count
   end
 end
