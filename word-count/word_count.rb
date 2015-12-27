5d7f5ab9b1e2fa6f18d005b4347ae82b05c3d0a7
Matt Ewell <matt.ewell@gmail.com>
2015-12-27 12:40:51 -0700

    Factor out phrase splitter

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,9 +1,16 @@
 class Phrase
+  attr_reader :phrase
+
   def initialize(phrase)
     @phrase = phrase
   end
+
+  def to_words(phrase)
+    phrase.downcase.split(/[ \n,]/)
+  end
+
   def word_count
-    words = @phrase.downcase.split(/[ \n,]/)
+    words = to_words(self.phrase)
     words.each_with_object(Hash.new(0)) do |word, words_count|
       scrubbed_word = word.match(/[\w']+/).to_s
       words_count[scrubbed_word] += 1 if scrubbed_word.length > 0
