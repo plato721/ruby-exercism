7e759c337c36b7bc6b42a11c0145c85a389cc903
Matt Ewell <matt.ewell@gmail.com>
2016-01-02 22:38:13 -0700

    Change symbols to strings in DNA_RNA

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,22 +1,22 @@
 module Complement
 
   DNA_RNA = {
-    :G => :C,
-    :C => :G,
-    :T => :A,
-    :A => :U
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
   }
 
   RNA_DNA = DNA_RNA.invert
 
   def self.dna_rna_map(nucleotide)
-    raise ArgumentError if DNA_RNA[nucleotide.to_sym].nil?
-    DNA_RNA[nucleotide.to_sym].to_s
+    raise ArgumentError if DNA_RNA[nucleotide].nil?
+    DNA_RNA[nucleotide].to_s
   end
 
   def self.rna_dna_map(nucleotide)
-    raise ArgumentError if RNA_DNA[nucleotide.to_sym].nil?
-    RNA_DNA[nucleotide.to_sym].to_s
+    raise ArgumentError if RNA_DNA[nucleotide].nil?
+    RNA_DNA[nucleotide].to_s
   end
 
   def self.of_dna(dna)
