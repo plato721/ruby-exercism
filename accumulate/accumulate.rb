ab21c769c5ccab855d39d9dfe2d089ed43aa03ce
Matt Ewell <matt.ewell@gmail.com>
2016-01-09 20:14:16 -0700

    Iterate over self instead of eaching through splatted range

--- accumulate/accumulate.rb
+++ accumulate/accumulate.rb
@@ -1,7 +1,7 @@
 class Array
   def accumulate
-    [*0...length].each_with_object([]) do |index, collection|
-      collection << yield(self[index])
+    self.each_with_object([]) do |element, collection|
+      collection << yield(element)
     end
   end
 end
