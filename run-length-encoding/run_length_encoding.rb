48456853067c9735e8f79f00166c595f185c62aa
Matt Ewell <matt.ewell@gmail.com>
2016-11-13 15:34:10 -0700

    use regex to count leading in encode

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -3,31 +3,23 @@ class RunLengthEncoding
 
   def self.encode(word)
     result = ""
-    word = word.split("")
     until word.empty?
       char = word[0]
       count = count_leading_character(word)
       count == 1 ? result += char : result += ( count.to_s + char )
-      count.times{ word.shift }
+      word.slice!(0,count)
     end
     result
   end
 
   def self.count_leading_character(word)
     char = word[0]
-    word = word.clone
-    count = 0
-    loop do
-      word[0] == char ? count += 1 : break
-      word.shift
-    end
-    count
+    word.match(/[#{char}]+/).to_s.size
   end
 
   def self.decode_single(code)
     matcher = code.match(/[\d]+/)
-    return code if !matcher
-    matcher.post_match * matcher[0].to_i
+    matcher ? matcher.post_match * matcher[0].to_i : code
   end
 
   def self.decode(word)
