61cabcd978c6f851655cbbf3f0641bfeb8d41ddc
Matt Ewell <matt.ewell@gmail.com>
2016-11-13 17:45:00 -0700

    initial complete bowling solution

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,19 +1,146 @@
 class Game
-  attr_accessor :score, :rolls
+  attr_accessor :frames
+
+  MIN_PINS = 0
+  MAX_PINS = 10
 
   def initialize
-    self.score = 0
-    self.rolls = []
+    self.frames = [ Frame.new(1) ]
   end
 
   def roll(pins)
-    self.rolls << pins
-    self.score += pins
-    self.score += pins if on_spare?
+    validate_completion
+    validate_pins(pins)
+    set_frames
+    assign_extras(pins)
+    assign_roll(pins)
   end
 
-  def on_spare?
-    return false if rolls.count < 3
-    rolls[-2] + rolls[-3] == 10
+  def validate_completion
+    if game_complete?
+      raise StandardError, "Game is already complete."
+    end
+  end
+
+  def validate_pins(pins)
+    check_min(pins)
+    check_max(pins)
+  end
+
+  def check_min(pins)
+    if pins > MAX_PINS
+      raise StandardError, "#{pins} pins is above max of #{MAX_PINS}"
+    end
+  end
+
+  def check_max(pins)
+    if pins < MIN_PINS
+      raise StandardError, "#{pins} pins is below min of #{MIN_PINS}"
+    end
+  end
+
+  def set_frames
+    count = self.frames.count
+    return if count >= 10
+    return if self.frames.last.need_roll?
+    self.frames << Frame.new(count + 1)
+  end
+
+  def assign_roll(pins)
+    self.frames.last.roll(pins) if self.frames.last.need_roll?
+  end
+
+  def assign_extras(pins)
+    self.frames.select{ |frame| frame.need_extra? }
+               .each{ |frame| frame.extra(pins) }
+  end
+
+  def score
+    validate_game_complete
+    self.frames.reduce(0) do |acc, frame|
+      acc + frame.score
+    end
+  end
+
+  def validate_game_complete
+    if !game_complete?
+      raise StandardError, "Cannot score incomplete game"
+    end
+  end
+
+  def game_complete?
+    (self.frames.count == 10) &&
+    !self.frames.last.need_roll? &&
+    !self.frames.last.need_extra?
+  end
+end
+
+class BookKeeping
+  VERSION = 2
+end
+
+class Frame
+  attr_accessor :spare, :strike, :roll_1, :roll_2, :extra_1, :extra_2, :number
+
+  def initialize(number)
+    raise ArgumentError if number > 10
+    self.number = number
+  end
+
+  def roll(pins)
+    validate_roll(pins)
+    !self.roll_1 ? self.roll_1 = pins : self.roll_2 = pins
+    check_strike_spare
+  end
+
+  def validate_roll(pins)
+    if self.roll_1 && self.roll_1 + pins > 10
+      raise StandardError, "Exceeds frame maximum"
+    end
+  end
+
+  def check_strike_spare
+    return self.strike = true if is_strike?
+    self.spare = true if is_spare?
+  end
+
+  def is_strike?
+    self.roll_1 == 10
+  end
+
+  def is_spare?
+    [self.roll_1, self.roll_2].compact.reduce(:+) == 10
+  end
+
+  def extra(pins)
+    validate_extra(pins)
+    !self.extra_1 ? self.extra_1 = pins : self.extra_2 = pins
+  end
+
+  def validate_extra(pins)
+    if self.number == 10 &&
+       self.strike &&
+       self.extra_1 &&
+       self.extra_1 != 10 &&
+       self.extra_1 + pins > 10
+      raise StandardError, "Exceeds bonus maximum"
+    end
+  end
+
+  def need_roll?
+    return false if self.roll_1 && self.roll_2
+    return false if self.strike
+    true
+  end
+
+  def need_extra?
+    return false if !self.spare && !self.strike
+    return false if self.spare && self.extra_1
+    return false if self.extra_1 && self.extra_2
+    true
+  end
+
+  def score
+    [self.roll_1, self.roll_2, self.extra_1, self.extra_2].compact.reduce(:+)
   end
 end
--- bowling/bowling_test.rb
+++ bowling/bowling_test.rb
@@ -38,67 +38,56 @@ class BowlingTest < Minitest::Test
   end
 
   def test_consecutive_spares_each_get_a_one_roll_bonus
-    skip
     roll([5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 31, @game.score
   end
 
   def test_a_spare_in_the_last_frame_gets_a_one_roll_bonus_that_is_counted_once
-    skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7])
     assert_equal 17, @game.score
   end
 
   def test_a_strike_earns_ten_points_in_frame_with_a_single_roll
-    skip
     roll([10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 10, @game.score
   end
 
   def test_points_scored_in_the_two_rolls_after_a_strike_are_counted_twice_as_a_bonus
-    skip
     roll([10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 26, @game.score
   end
 
   def test_consecutive_strikes_each_get_the_two_roll_bonus
-    skip
     roll([10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 81, @game.score
   end
 
   def test_a_strike_in_the_last_frame_gets_a_two_roll_bonus_that_is_counted_once
-    skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 1])
     assert_equal 18, @game.score
   end
 
   def test_rolling_a_spare_with_the_two_roll_bonus_does_not_get_a_bonus_roll
-    skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3])
     assert_equal 20, @game.score
   end
 
   def test_strikes_with_the_two_roll_bonus_do_not_get_bonus_rolls
-    skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])
     assert_equal 30, @game.score
   end
 
   def test_a_strike_with_the_one_roll_bonus_after_a_spare_in_the_last_frame_does_not_get_a_bonus
-    skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10])
     assert_equal 20, @game.score
   end
 
   def test_all_strikes_is_a_perfect_game
-    skip
     roll([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
     assert_equal 300, @game.score
   end
 
   def test_rolls_can_not_score_negative_points
-    skip
     assert_raises StandardError do
       roll([-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
       @game.score
@@ -106,7 +95,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_a_roll_can_not_score_more_than_10_points
-    skip
     assert_raises StandardError do
       roll([11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
       @game.score
@@ -114,7 +102,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_two_rolls_in_a_frame_can_not_score_more_than_10_points
-    skip
     assert_raises StandardError do
       roll([5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
       @game.score
@@ -122,7 +109,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_two_bonus_rolls_after_a_strike_in_the_last_frame_can_not_score_more_than_10_points
-    skip
     assert_raises StandardError do
       roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 5, 6])
       @game.score
@@ -130,7 +116,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_an_unstarted_game_can_not_be_scored
-    skip
     assert_raises StandardError do
       roll([])
       @game.score
@@ -138,7 +123,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_an_incomplete_game_can_not_be_scored
-    skip
     assert_raises StandardError do
       roll([0, 0])
       @game.score
@@ -146,7 +130,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_a_game_with_more_than_ten_frames_can_not_be_scored
-    skip
     assert_raises StandardError do
       roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
       @game.score
@@ -154,7 +137,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_bonus_rolls_for_a_strike_in_the_last_frame_must_be_rolled_before_score_can_be_calculated
-    skip
     assert_raises StandardError do
       roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10])
       @game.score
@@ -162,7 +144,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_both_bonus_rolls_for_a_strike_in_the_last_frame_must_be_rolled_before_score_can_be_calculated
-    skip
     assert_raises StandardError do
       roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10])
       @game.score
@@ -170,7 +151,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_bonus_roll_for_a_spare_in_the_last_frame_must_be_rolled_before_score_can_be_calculated
-    skip
     assert_raises StandardError do
       roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3])
       @game.score
@@ -195,7 +175,6 @@ class BowlingTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 2, BookKeeping::VERSION
   end
 end
