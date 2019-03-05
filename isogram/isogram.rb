aefe87126dcf9665d193dff9c133159bbf7d360c
plato721 <matt.ewell@gmail.com>
2019-03-05 14:02:05 -0700

    Refactor isogram solution

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,23 +1,11 @@
+# Determines if a word or phrase is an isogram.
+# An isogram is a word or phrase without repeating characters,
+# insensitve of case, ignoring hyphens or spaces.
 class Isogram
   class << self
     def isogram?(word)
-      word.split('').all? do |letter|
-        appears_exactly_once?(letter, word) || is_repeatable?(letter)
-      end
-    end
-
-    def appears_exactly_once?(letter, word)
-      count_letter_in_word(letter, word) == 1
-    end
-
-    def count_letter_in_word(letter, word)
-      word.split('').count do |c|
-        c.downcase == letter.downcase
-      end
-    end
-
-    def is_repeatable?(character)
-      character.match(/[- ]/)
+      normalized = word.scan(/[^- ]/)
+      normalized.uniq(&:downcase).length == normalized.length
     end
   end
 end
