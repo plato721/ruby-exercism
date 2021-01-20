a469b47436c7d0ae115942a66ad75b5868674724
Matt Ewell <matt.ewell@gmail.com>
2021-01-19 19:13:42 -0700

    Remove singleton
    Still memoize the calculation result

--- armstrong-numbers/armstrong_numbers.rb
+++ armstrong-numbers/armstrong_numbers.rb
@@ -1,21 +1,17 @@
 class ArmstrongCalculator
-  def self.singleton
-    @@instance ||= new
+  def initialize(max = 10_000_000)
+    @max = max
   end
 
-  def compute
-    @armstrong_numbers ||= [*0..10_000_000].keep_if { |num| armstrong?(num) }
+  def armstrong_numbers
+    @armstrong_numbers ||= [*0..@max].keep_if { |num| armstrong_number?(num) }
   end
 
-  private
-
-  def initialize; end
-
-  def armstrong?(num)
+  def armstrong_number?(num)
     number_string = num.to_s
     power = number_string.length
     num == number_string.split('').inject(0) { |memo, num_char| memo + num_char.to_i**power }
   end
 end
 
-ArmstrongNumbers = ArmstrongCalculator.singleton.compute
+ArmstrongNumbers = ArmstrongCalculator.new.armstrong_numbers
