187218a3141c75112685598edc70ccaed9e7f424
Matt Ewell <matt.ewell@gmail.com>
2023-08-19 06:52:29 -0600

    Refactor

--- simple-calculator/simple_calculator.rb
+++ simple-calculator/simple_calculator.rb
@@ -1,45 +1,43 @@
 class SimpleCalculator
+  class UnsupportedOperation < StandardError; end
+
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
-  def self.calculate(first_operand, second_operand, operation)
-    validate_arguments(first_operand, operation, second_operand)
-    result = get_result(first_operand, operation, second_operand)
-    format_result(first_operand, second_operand, operation, result)
-  rescue ZeroDivisionError
-    "Division by zero is not allowed."
-  end
-
-  def self.valid_operands?(operands)
-    operands.all? { |a| valid_operand?(a) }
-  end
-
-  def self.valid_operand?(operand)
-    operand.is_a?(Integer)
-  end
-
-  def self.valid_operation?(operation)
-    ALLOWED_OPERATIONS.include?(operation)
-  end
-
-  class UnsupportedOperation < StandardError
-    def initialize(msg = nil)
-      super
+  class << self
+    def calculate(first_operand, second_operand, operation)
+      validate_arguments(first_operand, operation, second_operand)
+      result = get_result(first_operand, operation, second_operand)
+      get_result_text(first_operand, second_operand, operation, result)
+    rescue ZeroDivisionError
+      "Division by zero is not allowed."
     end
-  end
 
-  private
+    private
 
-  def self.format_result(first_operand, second_operand, operation, result)
-    "#{first_operand} #{operation} #{second_operand} = #{result}"
-  end
+    def get_result_text(first_operand, second_operand, operation, result)
+      "#{first_operand} #{operation} #{second_operand} = #{result}"
+    end
 
-  def self.get_result(first_operand, operation, second_operand)
-    result = first_operand.to_i.send(operation.to_sym, second_operand.to_i)
-  end
+    def get_result(first_operand, operation, second_operand)
+      first_operand.to_i.send(operation.to_sym, second_operand.to_i)
+    end
 
-  def self.validate_arguments(first_operand, operation, second_operand)
-    raise ArgumentError unless valid_operands?([first_operand, second_operand])
-    raise UnsupportedOperation unless valid_operation?(operation)
+    def validate_arguments(first_operand, operation, second_operand)
+      raise ArgumentError unless valid_operands?(first_operand, second_operand)
+      raise UnsupportedOperation unless valid_operation?(operation)
+    end
+
+    def valid_operands?(*operands)
+      operands.all? { |o| valid_operand?(o) }
+    end
+
+    def valid_operand?(operand)
+      operand.is_a?(Integer)
+    end
+
+    def valid_operation?(operation)
+      ALLOWED_OPERATIONS.include?(operation)
+    end
   end
 end
 
