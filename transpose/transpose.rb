ac7f5d5ee12ef6b6c8007ddd470863a1695e42aa
Matt Ewell <matt.ewell@gmail.com>
2023-08-20 15:53:12 -0600

    Add initial transpose solution

--- transpose/transpose.rb
+++ transpose/transpose.rb
@@ -1,7 +1,26 @@
-=begin
-Write your code for the 'Transpose' exercise in this file. Make the tests in
-`transpose_test.rb` pass.
+class Transpose
+  def self.transpose(matrix)
+    return "" if matrix == ""
 
-To get started with TDD, see the `README.md` file in your
-`ruby/transpose` directory.
-=end
+    rows = matrix.split("\n")
+    rows = pad(rows)
+    rows.inject(Array.new {""}) do |transposed, row|
+      row.split("").each_with_index { |char, i| (transposed[i] ||= "" ) << char }
+      transposed
+    end.join("\n")
+  end
+
+  def self.pad(rows)
+    current_max = rows[-1].length
+    rows.reverse.each_with_index do |row, i|
+      rows[-i-1] = fill_row(rows[-i-1], current_max) if row.length < current_max
+      current_max = row.length if row.length > current_max
+    end
+    rows
+  end
+
+  def self.fill_row(row, length)
+    characters_needed = length - row.length
+    row + " " * characters_needed
+  end
+end
