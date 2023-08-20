7830ca7d3b605f7aae720d28350b8b08dc01f1d7
Matt Ewell <matt.ewell@gmail.com>
2023-08-20 09:55:18 -0600

    Add initial moviegoer solution

--- moviegoer/moviegoer.rb
+++ moviegoer/moviegoer.rb
@@ -1,23 +1,42 @@
-# This is a custom exception that you can use in your code
 class NotMovieClubMemberError < RuntimeError
 end
 
 class Moviegoer
+  ADULT_MIN_AGE = 18
+  SENIOR_DISCOUNT_MIN_AGE = 60
+  STANDARD_TICKET_PRICE = 15
+  DISCOUNT_TICKET_PRICE = 10
+
   def initialize(age, member: false)
     @age = age
     @member = member
   end
 
   def ticket_price
-    raise 'Please implement the Moviegoer#ticket_price method'
+    senior? ? DISCOUNT_TICKET_PRICE : STANDARD_TICKET_PRICE
+  end
+
+  private def senior?
+    age >= SENIOR_DISCOUNT_MIN_AGE
   end
 
   def watch_scary_movie?
-    raise 'Please implement the Moviegoer#watch_scary_movie method'
+    adult?
   end
 
-  # Popcorn is 🍿
-  def claim_free_popcorn!
-    raise 'Please implement the Moviegoer#claim_free_popcorn method'
+  private def adult?
+    age >= ADULT_MIN_AGE
   end
+
+  def claim_free_popcorn!
+    member? ? "🍿" : raise(NotMovieClubMemberError, "No 🍿 for you.")
+  end
+
+  def member?
+    member
+  end
+
+  private
+
+  attr_reader :age, :member
 end
