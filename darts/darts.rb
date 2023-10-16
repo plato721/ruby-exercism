813a0364f1ec07a16e8af44a83b8256ff90e1c91
Matt Ewell <matt.ewell@gmail.com>
2023-10-15 18:06:25 -0600

    Refactor
    There's many ways to do this, and I really don't know what's best.

--- darts/darts.rb
+++ darts/darts.rb
@@ -1,25 +1,30 @@
 class Darts
   attr_reader :x, :y
 
+  INNER_RING_DISTANCE = 1.0
+  MIDDLE_RING_DISTANCE = 5.0
+  OUTER_RING_DISTANCE = 10.0
+
   def initialize(x, y)
     @x = x
     @y = y
   end
 
   def score
-    case distance_from_center
-    when 0..1.0
-      10
-    when 1..5.0
-      5
-    when 5..10.0
-      1
-    else
-      0
+    return 0 unless distance_from_center <= OUTER_RING_DISTANCE
+
+    point_map.each do |ring_boundary, points|
+      return points if distance_from_center <= ring_boundary
     end
   end
 
   private
+
+  def point_map
+    { INNER_RING_DISTANCE => 10,
+      MIDDLE_RING_DISTANCE => 5,
+      OUTER_RING_DISTANCE => 1 }
+  end
 
   def distance_from_center
     Math.sqrt(x**2 + y**2)
