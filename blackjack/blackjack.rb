f7570d9953a6a7022a2ad3465d8a92ada8c90133
Matt Ewell <matt.ewell@gmail.com>
2024-01-07 11:48:53 -0700

    Add working blackjack

--- blackjack/blackjack.rb
+++ blackjack/blackjack.rb
@@ -3,9 +3,7 @@ module Blackjack
     def parse_card(card)
       case card
       when "ace" then 11
-      when "king", "queen", "jack"
-        10
-      when "ten" then 10
+      when "king", "queen", "jack", "ten" then 10
       when "nine" then 9
       when "eight" then 8
       when "seven" then 7
@@ -14,17 +12,36 @@ module Blackjack
       when "four" then 4
       when "three" then 3
       when "two" then 2
-      else
-        0
+      else 0
       end
     end
 
+    def sum_cards(*cards)
+      cards.sum { |c| parse_card(c) }
+    end
+
     def card_range(card1, card2)
-      raise "Please implement the Blackjack.card_range method"
+      case sum_cards(card1, card2)
+      when 4..11 then "low"
+      when 12..16 then "mid"
+      when 17..20 then "high"
+      else "blackjack"
+      end
     end
 
     def first_turn(card1, card2, dealer_card)
-      raise "Please implement the Blackjack.first_turn method"
+      total = sum_cards(card1, card2)
+      dealer_total = sum_cards(dealer_card)
+      range = card_range(card1, card2)
+      case
+      when total == 22 then "P"
+      when range == "high" then "S"
+      when range == "mid" && dealer_total > 6 then "H"
+      when range == "mid" then "S"
+      when range == "low" then "H"
+      when total == 21 && dealer_total == 11 then "S"
+      else "W"
+      end
     end
   end
 end
