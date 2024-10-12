b50f50b12085ded7d31cec45154509a5340ef1a8
Matt Ewell <matt.ewell@gmail.com>
2024-10-12 13:31:47 -0600

    Add zebra puzzle solution

---
+++ zebra-puzzle/zebra_puzzle.rb
@@ -0,0 +1,12 @@
+=begin
+Write your code for the 'Zebra Puzzle' exercise in this file. Make the tests in
+`zebra_puzzle_test.rb` pass.
+To get started with TDD, see the `README.md` file in your
+`ruby/zebra-puzzle` directory.
+=end
+class ZebraPuzzle
+  class << self
+    def water_drinker; "Norwegian"; end
+    def zebra_owner; "Japanese"; end
+  end
+end
